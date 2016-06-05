81514f915bd7621eb050d97f40a8077027e486cd
sigu <sigumagwa@gmail.com>
2016-06-05 03:57:10 +0300

    show full name on show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   validates :first_name, length: {minimum:3}, presence: true
   validates :last_name, length: {minimum:3}, presence: true
   validates :github, uniqueness: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
