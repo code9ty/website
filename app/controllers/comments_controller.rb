6c0358cf957dcc13d86650b17f78b8e6c17d3b71
sigu <sigumagwa@gmail.com>
2016-06-06 17:34:59 +0300

    validate comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,18 @@
 class CommentsController < ApplicationController
   def create
     user = User.find(params[:user_id])
-    # comment = Comment.new(comment_params)
-    binding.pry
     if user.comments.create(comment_params)
       redirect_to user
     else
       redirect_to user
-      console
+      binding.pry
     end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:learnt, :problem, :rating, :user_id)
+    params.require(:comment).permit(:learnt, :problem, :rating, 
+                                    :user_id, :suggestions, :possible_fix)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
+  validates :learnt, length: { minimum: 10 }
+  validates :problem, length: { minimum: 10 }
+  validates :suggestions, length: { minimum: 10 }
+  validates :possible_fix, length: { minimum: 10 }
 end
