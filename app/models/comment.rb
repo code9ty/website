f4f4d305e68eaad56f985ea5d41dfc9f24c827ae
sigu <sigumagwa@gmail.com>
2016-06-09 21:31:28 +0300

    descending comments display

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates :learnt, length: { minimum: 10 }
   validates :problem, length: { minimum: 10 }
   validates :suggestions, length: { minimum: 10 }
