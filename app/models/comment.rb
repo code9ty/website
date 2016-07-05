120b146516e4766321676dbedd6236e038aa6cd0
Cess <cessmbuguar@gmail.com>
2016-07-05 23:17:48 +0300

    allow blank suggestions and problems

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :learnt, length: { minimum: 10 }
-  validates :problem, length: { minimum: 10 }
-  validates :suggestions, length: { minimum: 10 }
-  validates :possible_fix, length: { minimum: 10 }
+ validates :problem, length: { minimum: 10 }, allow_blank: true
+ validates :suggestions, length: { minimum: 10 }, allow_blank: true
+ validates :possible_fix, length:  { minimum: 10 }, allow_blank: true
 end
