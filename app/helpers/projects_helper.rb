8072a8ecf9e166b83ec2e2a323a5fa28b9c4dc57
Sigu <sigumagwa@gmail.com>
2016-07-10 18:31:47 +0300

    do not calculate for negative dates

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,22 +4,37 @@ module ProjectsHelper
   end
 
   def remaining_days project
-    project.due_date - Time.zone.now
+    if project.due_date.future?
+      # project.due_date - Time.zone.now
+      distance_of_time_in_words(Time.zone.now, project.due_date)
+    else
+      0
+    end
   end
 
   def percentage_remaining project
-   percentage = (remaining_days(project)/max_days(project))*100
-   percentage.to_i
+    if project.due_date.future?
+       percentage = ((project.due_date - Time.zone.now)/max_days(project))*100
+       if percentage.to_i >=  0
+         percentage.to_i
+       else
+         0
+       end
+    else
+      0
+    end
   end
 
   def coloring project
-    percentage = percentage_remaining project
-    if percentage < 33
-      "alert"
-    elsif percentage < 66
-      "warning"
-    else
-      "success"
+    if project.due_date.future?
+      percentage = percentage_remaining project
+      if percentage < 33
+        "alert"
+      elsif percentage < 66
+        "warning"
+      else
+        "success"
+      end
     end
   end
 end
