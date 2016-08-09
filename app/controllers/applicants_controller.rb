7a56e6fc5b0c79a78a7fb315bd67c38a5d0fe006
Cess <cessmbuguar@gmail.com>
2016-08-09 10:46:17 +0300

    generate controller for applicants

---
+++ app/controllers/applicants_controller.rb
@@ -0,0 +1,31 @@
+class ApplicantsController < ApplicationController
+  before_action 
+  def new
+    @applicant = Applicant.new
+  end
+
+  def create
+    @applicant = Applicant.new(apply_params)
+    if @applicant.save
+      flash.now[:success] = "Application sent an email will be sent to you if you are choosen"
+      render 'show'
+    else
+      flash.now[:alert] = "Error saving form"
+      render 'new'
+    end
+  end
+  def show
+    @applicant = Applicant.find(params[:id])
+  end
+  def index
+    @applicants = Applicant.all
+  end
+  def accept
+    @accepts = Applicant.find(params[:geek])
+    flash.now[:success] = "Good choice, an email was sent to the applicant"
+  end
+  private
+  def apply_params
+    params.require(:applicant).permit(:name, :email, :github)
+  end
+end
---
+++ spec/factories/applicants.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :applicant do
+    name "MyString"
+    email "MyString"
+    github "MyString"
+  end
+end
