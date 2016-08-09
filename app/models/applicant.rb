bf8f5ef0c22ea45dc7d94ec020f8658694a1cce6
Cess <cessmbuguar@gmail.com>
2016-08-09 10:43:43 +0300

    validating applicants input

---
+++ app/models/applicant.rb
@@ -0,0 +1,8 @@
+class Applicant < ApplicationRecord
+  REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :email, presence: true, 
+    format: { with: REGEX}, uniqueness: true
+  validates :github, presence: true, length: { minimum:15 },
+    uniqueness: true
+end
