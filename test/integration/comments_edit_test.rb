2d27f2469bef80e7a3a7b0aa1fb7549d7a1521db
kevin <kevinochieng548@gmail.com>
2016-08-12 14:35:25 +0300

    test redirect for non-existent feedback

--- test/integration/comments_edit_test.rb
+++ test/integration/comments_edit_test.rb
@@ -3,20 +3,22 @@ require 'test_helper'
 class CommentsEditTest < ActionDispatch::IntegrationTest
   
   def setup
-    @user = users(:majaribu)
+    @user = users(:yulemzi)
+    @user2 = users(:einstein)
+    @comment = comments(:comment1)
   end
 
   test "redirect edit of nonexistent feedback" do
-    log_in_as(@user)
-    last_feedback = @user.comments.first
+    log_in_as(@user2)
+    last_feedback = @user2.comments.first
     assert last_feedback.nil?
-    assert_redirected_to @user
+    assert_redirected_to @user2
     assert_not flash.empty?
   end
 
   test "successful edit feedback" do
     log_in_as(@user)
-    last_feedback = @user.comments.first
+    last_feedback = @comment
     assert_not last_feedback.nil?
     get edit_user_comment_path(@user)
     assert_template 'comments/edit'
