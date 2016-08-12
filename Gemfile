82c295fd3e06ebd3dc07132ac03537594a445a1f
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-12 08:30:06 +0300

    add gem letter_opener

--- Gemfile
+++ Gemfile
@@ -42,7 +42,7 @@ group :production do
 end
 
 group :development do
-  # gem "letter_opener"
+  gem "letter_opener"
   gem 'web-console', github: 'rails/web-console'
   gem 'listen', '~> 3.0.5'
   gem 'byebug'
--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -1,14 +1,10 @@
 class ApplicantMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.applicant_mailer.applicant_accept.subject
-  #
   def applicant_accept(applicant)
     @applicant = applicant
     mail to: applicant.email, subject: "Application Accepted"
   end
+
   def application_request(applicant)
     @applicant = applicant
     mail to: appplicant.email, subject: "Application Request"
