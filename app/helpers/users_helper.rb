95fcdaccf4f29958c5d4df21761ec40e62821dff
AndyCodez <andystudy4@gmail.com>
2016-08-13 10:28:14 +0300

    Add gravatar in orbit slider

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,19 @@
 module UsersHelper
   # Returns the Gravatar for the given user.
   def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    if user
+      email = user.email.downcase
+      name = user.full_name
+    else
+      name = '#code90'
+      email = 'example@me.com'
+    end
+    gravatar_id = Digest::MD5::hexdigest(email)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.full_name, class: "gravatar")
+    image_tag(gravatar_url, alt: name, class: "gravatar")
+  end
+
+  def find_me(email)
+    User.find_by(email: email)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ user = User.create!(
  year_of_study: "4",
  github: "https://github.com/Makatunga",
  mobile_number: "0712345678",
- email: "makatunga@makatunga.com",
+ email: "andrewhenry08@gmail.com",
  password: "cartoon",
  password_confirmation: "cartoon",
  treasurer: false,
@@ -39,15 +39,15 @@ project = Project.create!(
   title: "Test Project",
   link: Faker::Internet.url,
   # due_date: 2.days.from_now,
-  start_date: 10.days.ago
+  # start_date: 10.days.ago
 )
 user.projects = [project]
 5.times do
   title = Faker::Commerce.product_name
   link = Faker::Internet.url
-  due_date = Faker::Date.between(Date.today, 28.days.from_now)
-  start_date = Faker::Date.between(4.days.ago, Time.zone.now)
-  Project.create!(title: title, link: link, due_date: due_date, start_date: start_date)
+  # due_date = Faker::Date.between(Date.today, 28.days.from_now)
+  # start_date = Faker::Date.between(4.days.ago, Time.zone.now)
+  Project.create!(title: title, link: link)
 end
 # 5 times user ids
 1.upto(4) do |n|
