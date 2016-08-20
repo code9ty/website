9773a9579871261a52983b9cf428c51090c9fccb
Sigu <sigumagwa@gmail.com>
2016-08-20 10:15:41 +0300

    show only visible users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,13 @@ class UsersController < ApplicationController
     @users = User.all
   end
   def pride
-    @users = User.all
+    @users = User.where(visible: true)
   end
   def new
     if params[:email]
       applicant = Applicant.find_by_email(params[:email])
       if applicant.accept?
-        @user = applicant 
+        @user = applicant
         @disabled = true
       else
         redirect_to apply_path
@@ -60,12 +60,12 @@ class UsersController < ApplicationController
                                 :year_of_study, :github, :mobile_number,
                                 :email, :password, :password_confirmation)
   end
-  # return true if current user 
+  # return true if current user
   def correct_user
     if current_user
       @user = User.find(params[:id]) if params[:id]
       unless current_user?(@user) || current_user.admin?
-        redirect_to user_path(current_user) 
+        redirect_to user_path(current_user)
       end
     else
       redirect_to login_path
