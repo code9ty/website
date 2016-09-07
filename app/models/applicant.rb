668b1139ec9254e9bf1a7ece11356cebd474b872
Sigu <sigumagwa@gmail.com>
2016-09-07 14:45:50 +0300

    begin africastlking integration

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -4,9 +4,23 @@ class Applicant < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :mobile_number, presence: true, numericality: true
-  validates :email, presence: true, 
+  validates :email, presence: true,
     format: { with: REGEX}, uniqueness: true
   validates :github, presence: true, length: { minimum:15 },
     uniqueness: true
   enum status: { apply: 0, accept: 1 }
+  def send_sms(message)
+    username = ENV['AT_USERNAME']
+    apikey = ENV['AT_API']
+    gateway = AfricasTalkingGateway.new(username, apikey)
+    begin
+      reports = gateway.sendMessage(mobile_number, message)
+      reports.each {|x|
+        # status is either "Success" or "error message"
+        puts 'number=' + x.mobile_number + ';status=' + x.status + ';messageId=' + x.messageId + ';cost=' + x.cost
+      }
+    rescue AfricasTalkingGatewayException => ex
+      puts 'Encountered an error: ' + ex.message
+    end
+  end
 end
