7b697fe9196e3e54aeaf3bce961a83269bd2704a
Sigu <sigumagwa@gmail.com>
2016-09-07 17:31:56 +0300

    send sms to accepted applicant

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,4 +1,5 @@
 class Applicant < ApplicationRecord
+  require 'AfricasTalkingGateway'
   attr_reader :university, :year_of_study
   REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :first_name, presence: true
@@ -12,12 +13,17 @@ class Applicant < ApplicationRecord
   def send_sms(message)
     username = ENV['AT_USERNAME']
     apikey = ENV['AT_API']
+    #introduce country code into mobile number if the previous number did not have it
+    first_character = mobile_number[0] 
+    if first_character == '0'
+      mobile_number[0] = '+254'
+    end
     gateway = AfricasTalkingGateway.new(username, apikey)
     begin
       reports = gateway.sendMessage(mobile_number, message)
       reports.each {|x|
         # status is either "Success" or "error message"
-        puts 'number=' + x.mobile_number + ';status=' + x.status + ';messageId=' + x.messageId + ';cost=' + x.cost
+        puts 'number=' + x.number + ';status=' + x.status + ';messageId=' + x.messageId + ';cost=' + x.cost
       }
     rescue AfricasTalkingGatewayException => ex
       puts 'Encountered an error: ' + ex.message
--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Feedback
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.autoload_paths += %W(#{config.root}/lib)
-    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
 end
