f2c0dcc6484d221e23361f4266f2118269f978d7
StlMaris123 <stellamarisnjage@gmail.com>
2016-09-19 20:09:03 +0300

    integration test for ssignment mailer

---
+++ test/integration/new_assignment_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class NewAssignmentTest < ActionDispatch::IntegrationTest
+  def setup
+    @admin = users(:majaribu)
+  end
+
+  test "new assignment including new delivery " do
+    log_in_as(@admin)
+    get new_assignment_url
+    assert_difference 'Assignment.count', 1 do
+      post assignments_path, params:{ assignment: { title: "Learn Ruby Hard Way",
+                                                 description: "Do all the exercises",
+                                                 link: "https://learnrubythehardway.com",
+                                                 due_date: "2016-09-17 08:12:29 UTC ",
+                                                 start_date: "2016-10-17 08:12:29 UTC"
+      } }
+    end
+    assert_redirected_to new_assignment_path
+    follow_redirect!
+    assert_template 'new'
+    assert_not flash.empty?
+    assert_equal User.count , ActionMailer::Base.deliveries.size
+  end
+end
