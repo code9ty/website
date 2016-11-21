6c6d989e4444ab6ffb2f91985aea316837e9579d
sigu <sigumagwa@gmail.com>
2016-11-21 17:58:13 +0300

    open graph testing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,11 @@ class StaticPagesController < ApplicationController
     @applicant = Applicant.new
     @open = Intake.open?
   end
+  def what
+    set_meta_tags og: {
+      image:'https://s3-us-west-2.amazonaws.com/code9ty/edt.jpg',
+      url: learn_to_code_url,
+      type: 'website'
+    }
+  end
 end
